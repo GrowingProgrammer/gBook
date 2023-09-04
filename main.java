1ba7f9ead97bf1e1b735a8c0f29e9e48f069dc10
GrowingProgrammer <141649888+GrowingProgrammer@users.noreply.github.com>
2023-09-04 21:02:08 +0900

    Github 수정 Test
    Github 수정 Test 1번째

--- main.java
+++ main.java
@@ -4,7 +4,7 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Github 연동 Test 중입니다.55");
+		System.out.println("Github 수정 Test");
 	}
 
 }
